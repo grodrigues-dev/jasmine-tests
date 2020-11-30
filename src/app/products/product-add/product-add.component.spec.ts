8edcf9aaf4ff988549ee493034081ac57ac9b9f7
Gabriel Rodrigues <gabriel.rodrigues@opah.com.br>
2020-11-30 20:41:04 -0300

    ajustando injeção de dependencias

--- src/app/products/product-add/product-add.component.spec.ts
+++ src/app/products/product-add/product-add.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { AppComponent } from 'src/app/app.component';
 
 import { ProductAddComponent } from './product-add.component';
 
-describe('ProductAddComponent', () => {
+fdescribe('ProductAddComponent', () => {
   let component: ProductAddComponent;
   let fixture: ComponentFixture<ProductAddComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductAddComponent ]
+      declarations: [ ProductAddComponent ],
+      imports: [
+        ReactiveFormsModule, 
+        ImageCropperModule, 
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
